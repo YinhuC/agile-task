ca0df88fbd5f1abfd00d37faa68c012f86105e81
YinhuC <yinhucui@gmail.com>
2023-06-13 21:49:52 +1200

    fix: Resolve bug by adding TypeOrmModule to category module
    - Import and configure TypeOrmModule to establish connection with the database
    - Ensure proper synchronization of entity schemas

--- server/src/category/category.module.ts
+++ server/src/category/category.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './controllers/category.controller';
 import { CategoryService } from './services/category.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './category.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService],
   exports: [CategoryService],
