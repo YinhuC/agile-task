52cab7cf086fce19a9a5aad1600af20e027a5de7
YinhuC <yinhucui@gmail.com>
2023-06-13 21:35:21 +1200

    feat: Add category controller, service, and DTOs
    - Create CategoryController to handle category-related API endpoints
    - Implement CategoryService for business logic and data manipulation
    - Define DTOs (Data Transfer Objects) for request and response validation
    - Wire up routes and controller methods for CRUD operations

--- server/src/category/controllers/category.controller.ts
+++ server/src/category/controllers/category.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { Category } from '../category.entity';
+import { CategoryService } from '../services/category.service';
+import { CreateCategoryDto } from '../dto/create-category.dto';
+import { UpdateCategoryDto } from '../dto/update-category.dto';
 
 @Controller('categories')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get(':id')
+  async getProjectCategories(@Param('id') id: number): Promise<Category[]> {
+    return await this.categoryService.getAllCategoriesByProjectId(id);
+  }
+
+  @Post()
+  async createCategory(
+    @Body() createCategoryDto: CreateCategoryDto
+  ): Promise<Category> {
+    return await this.categoryService.createCategory(createCategoryDto);
+  }
+
+  @Put(':id')
+  async updateCategory(
+    @Param('id') id: number,
+    @Body() updateCategoryDto: UpdateCategoryDto
+  ): Promise<Category> {
+    return await this.categoryService.updateCategory(id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  async deleteCategory(@Param('id') id: number): Promise<void> {
+    await this.categoryService.deleteCategory(id);
+  }
+}
--- server/src/category/services/category.service.ts
+++ server/src/category/services/category.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from '../category.entity';
+import { CreateCategoryDto } from '../dto/create-category.dto';
+import { UpdateCategoryDto } from '../dto/update-category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>
+  ) {}
+
+  async getAllCategoriesByProjectId(projectId: number): Promise<Category[]> {
+    return await this.categoryRepository.find({
+      where: { project: { id: projectId } },
+      relations: ['project'],
+    });
+  }
+
+  async createCategory(
+    createCategoryDto: CreateCategoryDto
+  ): Promise<Category> {
+    const category = this.categoryRepository.create(createCategoryDto);
+    return await this.categoryRepository.save(category);
+  }
+
+  async updateCategory(
+    id: number,
+    updateCategoryDto: UpdateCategoryDto
+  ): Promise<Category> {
+    const category = await this.categoryRepository.preload({
+      id,
+      ...updateCategoryDto,
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+
+    return await this.categoryRepository.save(category);
+  }
+
+  async deleteCategory(id: number): Promise<void> {
+    const result = await this.categoryRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+  }
+}
