5d7b547c590962251cd98f32c3667c32f135ba38
YinhuC <yinhucui@gmail.com>
2023-06-19 16:17:37 +1200

    feat: Add Redux store and implement provider pattern
    - Set up the Redux store for state management
    - Implement the provider pattern to provide the Redux store to the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { useLocation, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from './providers/ThemeProvider';
 import Header from './components/Header';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './store';
+
+type Props = {};
+
+function Providers({ children }: PropsWithChildren & Props) {
+  return (
+    <ReduxProvider store={store}>
+      <ThemeProvider>{children}</ThemeProvider>
+    </ReduxProvider>
+  );
+}
 
 function App() {
   const location = useLocation();
@@ -12,14 +24,14 @@ function App() {
   const isRegisterPage = location.pathname === '/register';
 
   return (
-    <ThemeProvider>
+    <Providers>
       {!isLoginPage && !isRegisterPage && <Header />}
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
       </Routes>
-    </ThemeProvider>
+    </Providers>
   );
 }
 
