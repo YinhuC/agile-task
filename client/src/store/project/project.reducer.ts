ad3d722d818908d4e672f46849d9085a8509bb5e
YinhuC <yinhucui@gmail.com>
2023-06-21 20:49:06 +1200

    feat: Add project slice
    - Define the project state shape and initial values
    - Create a project slice using createSlice from @reduxjs/toolkit
    - Implement actions for adding, removing, and updating projects
    - Handle asynchronous logic using extraReducers and project thunks
    - Update the state based on the API response

---
+++ client/src/store/project/project.reducer.ts
@@ -0,0 +1,70 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Project } from '../../types/project.types';
+import {
+  createProjectThunk,
+  deleteProjectThunk,
+  fetchProjectsThunk,
+  updateProjectThunk,
+} from './project.actions';
+
+export interface ProjectState {
+  projects: Project[];
+}
+
+const initialState: ProjectState = {
+  projects: [],
+};
+
+export const projectSlice = createSlice({
+  name: 'projects',
+  initialState,
+  reducers: {
+    addProject: (state, action: PayloadAction<Project>) => {
+      state.projects.unshift(action.payload);
+    },
+
+    removeProject: (state, action: PayloadAction<number>) => {
+      state.projects = state.projects.filter(
+        (project) => project.id !== action.payload
+      );
+    },
+
+    updateProject: (state, action: PayloadAction<Project>) => {
+      const index = state.projects.findIndex(
+        (project) => project.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.projects[index] = action.payload;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchProjectsThunk.fulfilled, (state, action) => {
+      state.projects = action.payload.data;
+    });
+
+    builder.addCase(createProjectThunk.fulfilled, (state, action) => {
+      state.projects.unshift(action.payload.data);
+    });
+
+    builder.addCase(updateProjectThunk.fulfilled, (state, action) => {
+      const index = state.projects.findIndex(
+        (project) => project.id === action.payload.data.id
+      );
+      if (index !== -1) {
+        state.projects[index] = action.payload.data;
+      }
+    });
+
+    builder.addCase(deleteProjectThunk.fulfilled, (state, action) => {
+      state.projects = state.projects.filter(
+        (project) => project.id !== action.payload.data.id
+      );
+    });
+  },
+});
+
+export const { addProject, removeProject, updateProject } =
+  projectSlice.actions;
+
+export default projectSlice.reducer;
