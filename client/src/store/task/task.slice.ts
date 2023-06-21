08c5a1c179dbd94a001fcbb96904272c2ffa4f2e
YinhuC <yinhucui@gmail.com>
2023-06-21 20:55:40 +1200

    feat: Add task slice
    - Define the task state shape and initial values
    - Create a task slice using createSlice from @reduxjs/toolkit
    - Implement actions for adding, removing, and updating tasks
    - Handle asynchronous logic using extraReducers and task thunks
    - Update the state based on the API response

---
+++ client/src/store/task/task.slice.ts
@@ -0,0 +1,67 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Task } from '../../types/task.types';
+import {
+  createTaskThunk,
+  deleteTaskThunk,
+  fetchTasksThunk,
+  updateTaskThunk,
+} from './task.actions';
+
+export interface TaskState {
+  tasks: Task[];
+}
+
+const initialState: TaskState = {
+  tasks: [],
+};
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<Task>) => {
+      state.tasks.unshift(action.payload);
+    },
+
+    removeTask: (state, action: PayloadAction<number>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+
+    updateTask: (state, action: PayloadAction<Task>) => {
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.tasks[index] = action.payload;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchTasksThunk.fulfilled, (state, action) => {
+      state.tasks = action.payload.data;
+    });
+
+    builder.addCase(createTaskThunk.fulfilled, (state, action) => {
+      state.tasks.unshift(action.payload.data);
+    });
+
+    builder.addCase(updateTaskThunk.fulfilled, (state, action) => {
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.data.id
+      );
+      if (index !== -1) {
+        state.tasks[index] = action.payload.data;
+      }
+    });
+
+    builder.addCase(deleteTaskThunk.fulfilled, (state, action) => {
+      state.tasks = state.tasks.filter(
+        (task) => task.id !== action.payload.data.id
+      );
+    });
+  },
+});
+
+export const { addTask, removeTask, updateTask } = tasksSlice.actions;
+
+export default tasksSlice.reducer;
