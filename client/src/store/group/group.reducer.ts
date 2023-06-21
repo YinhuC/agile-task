c6a29b5b4a922b96a535596663ececc9d54477ac
YinhuC <yinhucui@gmail.com>
2023-06-21 20:11:16 +1200

    feat: Add group slice and thunks
    - Define the group state shape and initial values
    - Create a group slice using createSlice from @reduxjs/toolkit
    - Implement actions for adding, removing, and updating groups
    - Handle asynchronous logic using extraReducers and fetchGroupsThunk
    - Update the state based on the API response

--- client/src/store/group/group.reducer.ts
+++ client/src/store/group/group.reducer.ts
@@ -4,10 +4,14 @@ import { fetchGroupsThunk } from './group.actions';
 
 export interface GroupState {
   groups: Group[];
+  isLoading: boolean;
+  error: string | null;
 }
 
 const initialState: GroupState = {
   groups: [],
+  isLoading: false,
+  error: null,
 };
 
 export const groupsSlice = createSlice({
@@ -17,14 +21,41 @@ export const groupsSlice = createSlice({
     addGroup: (state, action: PayloadAction<Group>) => {
       state.groups.unshift(action.payload);
     },
+
+    removeGroup: (state, action: PayloadAction<number>) => {
+      state.groups = state.groups.filter(
+        (group) => group.id !== action.payload
+      );
+    },
+
+    updateGroup: (state, action: PayloadAction<Group>) => {
+      const index = state.groups.findIndex(
+        (group) => group.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.groups[index] = action.payload;
+      }
+    },
   },
   extraReducers: (builder) => {
+    builder.addCase(fetchGroupsThunk.pending, (state) => {
+      state.isLoading = true;
+      state.error = null;
+    });
+
     builder.addCase(fetchGroupsThunk.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = null;
       state.groups = action.payload.data;
+    });
+
+    builder.addCase(fetchGroupsThunk.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message ?? 'Failed to fetch groups.';
     });
   },
 });
 
-export const { addGroup } = groupsSlice.actions;
+export const { addGroup, removeGroup, updateGroup } = groupsSlice.actions;
 
 export default groupsSlice.reducer;
