7fcf3cb47b7c9f8433afce7238a3174aa5bdc7f6
YinhuC <yinhucui@gmail.com>
2023-06-27 00:56:50 +1200

    chore: Update server package.json for migrations
    - Added necessary dependencies for database migrations
    - Configured migration scripts in package.json

---
+++ server/src/database/migrations/1687782274507-start.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class Start1687782274507 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
