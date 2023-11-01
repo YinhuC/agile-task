901202d4154d8916f94a3d7efc7965df2ceb3ccc
Yinhu Cui <yinhucui@MacChicken.local>
2023-11-01 20:46:13 +1300

    fix: Resolve package.json and configuration issues in main.ts
    - Refactored configurations in main.ts to address inconsistencies, ensuring correct settings for the application's startup.

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,32 +4,29 @@ import session from 'express-session';
 import passport from 'passport';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-
-const data: any = dotenv.parse(fs.readFileSync('../.env'));
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: data.REACT_APP_CLIENT_URL,
+    origin: process.env.REACT_APP_CLIENT_URL,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.use(
     session({
-      secret: data.REACT_APP_SECRET,
+      secret: process.env.REACT_APP_SECRET,
       resave: false,
       saveUninitialized: false,
       name: 'SESSION_ID',
-      proxy: true,
+      proxy: process.env.REACT_APP_NODE_ENV === 'production',
       cookie: {
         maxAge: 86400000,
-        secure: data.REACT_APP_NODE_ENV === 'production',
-        sameSite: data.REACT_APP_NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.REACT_APP_NODE_ENV === 'production',
+        sameSite:
+          process.env.REACT_APP_NODE_ENV === 'production' ? 'none' : 'lax',
         domain:
-          data.REACT_APP_NODE_ENV === 'production'
+          process.env.REACT_APP_NODE_ENV === 'production'
             ? '.onrender.com'
             : undefined,
       },
@@ -39,9 +36,9 @@ async function bootstrap() {
   app.use(passport.session());
 
   try {
-    await app.listen(data.REACT_APP_API_PORT || 3300, () => {
-      console.log(`Running on Port ${data.REACT_APP_API_PORT || 3300}`);
-      console.log(`API URL: ${data.REACT_APP_API_URL}`);
+    await app.listen(process.env.REACT_APP_API_PORT || 3300, () => {
+      console.log(`Running on Port ${process.env.REACT_APP_API_PORT || 3300}`);
+      console.log(`API URL: ${process.env.REACT_APP_API_URL}`);
     });
   } catch (err) {
     console.log(err);
