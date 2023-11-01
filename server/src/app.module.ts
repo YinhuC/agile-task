55f47c10a61137e7c5f7f8e06e19bb5a01ac97d1
Yinhu <41180818+YinhuC@users.noreply.github.com>
2023-11-01 21:55:36 +1300

    feat: integrate Config Module and reorganize module imports (#14)
    * Update README.md
    
    * feat: integrate Config Module and reorganize module imports
    
    - Integrated '@nestjs/config' to manage configuration variables across the application, enabling access to environment variables and other configuration sources.
    - Reordered module imports within the application to improve readability and maintain consistency by grouping and arranging modules based on their purpose or functionality.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,11 +7,11 @@ import { GroupModule } from './group/group.module';
 import { ProjectModule } from './project/project.module';
 import { CategoryModule } from './category/category.module';
 import { TaskModule } from './task/task.module';
-import { dataSourceOptions } from './database/data-source';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataSourceOptions),
+    ConfigModule.forRoot({ isGlobal: true }),
     PassportModule.register({
       session: true,
     }),
@@ -21,6 +21,20 @@ import { dataSourceOptions } from './database/data-source';
     ProjectModule,
     CategoryModule,
     TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.REACT_APP_DB_HOST,
+      port: parseInt(process.env.REACT_APP_DB_PORT),
+      username: process.env.REACT_APP_DB_USERNAME,
+      password: process.env.REACT_APP_DB_PASSWORD,
+      database: process.env.REACT_APP_DB_NAME,
+      // https://stackoverflow.com/questions/59435293/typeorm-entity-in-nestjs-cannot-use-import-statement-outside-a-module
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/database/migrations/**/*{.js,.ts}'],
+      synchronize: false,
+      logging: true,
+      poolSize: 5,
+    }),
   ],
   controllers: [],
   providers: [],
